ffbdd6320c21111f3cd3bf079621da87df0467a1
Iskender <113851731+IskeenX@users.noreply.github.com>
2023-03-15 22:14:41 +0100

    Particle Simulation

---
+++ ParticleSimulation/ParticleSimulation/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParticleSimulation
+{
+    class Program
+    {
+        static void Main()
+        {
+            Box domain = new Box(30, 20); //(width,height)
+            Simulation simulation = new Simulation(domain);
+            simulation.Populate(7);
+            simulation.Run();
+        }
+    }
+}
