baf9e56ac5be75b8fa800cf2f23d1d8d4b13cb54
Iskender <113851731+IskeenX@users.noreply.github.com>
2023-03-19 11:42:55 +0100

    OOP practise

---
+++ OOPbasics/OOP/Square.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    class Square
+    {
+        // Fields
+
+        private int x;
+        private int y;
+        private int width;
+        private int height;
+
+        // Constructor
+
+        public Square()
+        {
+            Random r = new Random();
+            this.x = r.Next(1, 21);
+            this.y = r.Next(1, 21);
+            this.width = r.Next(1, 11);
+            this.height = r.Next(1, 11);
+        }
+
+        public Square(int x, int y, int width, int height)
+        {
+            this.x = x;
+            this.y = y;
+            this.width = width;
+            this.height = height;
+        }
+    }
+}
---
+++ OOPbasics/OOP/Student.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    class Student
+    {
+        // Fields
+
+        private string name;
+        private string neptun;
+        public int age; // for the OldestStudent method
+
+        // Constructor
+
+        public Student()
+        {
+            name = "Anonymous";
+            neptun = "AAA111";
+            age = -1;
+        }
+
+        public Student(string name, string neptun, int age)
+        {
+            this.name = name;
+            this.neptun = neptun;
+            this.age = age;
+        }
+
+        public string PrintOut()
+        {
+            return "Name: " + name + ", Neptun code: " + neptun + ", age: " + age;
+        }
+    }
+}
