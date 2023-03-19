0bbcd73b9f6637016caf3910bd7b8035c56ffacb
Iskender <113851731+IskeenX@users.noreply.github.com>
2023-03-19 09:03:58 +0100

    OOP Practice

---
+++ InheritanceAndPolymorphism/InheritanceAndPolymorphism/Rectangle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceAndPolymorphism
+{
+    class Rectangle : Trapezoid
+    {
+        public Rectangle(float a, float b) : base(a, b)
+        {
+            ToString();
+        }
+        public Rectangle(float a) : base(a) { }
+
+        public override float Area()
+        {
+            float areaR = sideA * sideB;
+            return areaR;
+        }
+        public override float Perimeter()
+        {
+            float perimeterR = (sideA + sideB) * 2;
+            return perimeterR;
+        }
+
+        public override string ToString()
+        {
+            float area = Area();
+            float perimeter = Perimeter();
+            return $"This is a rectangle with perimeter {perimeter} and area {area}.";
+        }
+    }
+}
