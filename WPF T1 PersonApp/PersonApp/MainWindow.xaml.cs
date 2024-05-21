cf631f263e6e1a6168324c32a8a04d2fc41a9423
Iskender Degenbaev <113851731+IskeenX@users.noreply.github.com>
2024-05-21 03:13:20 +0200

    WPF

---
+++ WPF T1 PersonApp/PersonApp/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+
+namespace PersonApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void bttnSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"Hello {txtBxFirstName.Text} {txtBxLastName.Text}");
+        }
+    }
+}
